c8f472d9d43b06644b47233b56ff3217fb12f649
Sanjay <sanzaikc@gmail.com>
2022-06-22 08:40:31 +0545

    custom scrollbar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function About() {
   return (
     <div id="about" className="h-screen">
-      About
+      <div className="space-x-4 font-bold">
+        <span className="seconday-font text-2xl">1.</span>
+        <span className="text-3xl text-gray-200 ">About Me</span>
+      </div>
+      <div className="text-lg text-gray-400">Hi There, I'm Sanjay Khatri.</div>
     </div>
   );
 }
